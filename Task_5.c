d02ec17cf10d418b7c42ace18e732c05bda6e6d4
Ansh <anshkumar41523@gmail.com>
2026-09-16 21:36:09 -0700

    Add Task_5.c to read and display float number

---
+++ Task_5.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+int main(){
+    float number ;
+    printf("Enter a  Number in Float =");
+    scanf("%f",&number);
+    printf("\n%.1f",number);
+    printf("\n%.3f",number);
+
+}
