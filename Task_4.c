14d5e15c8e433acb5403a574a453dca407f5c5e8
Ansh <anshkumar41523@gmail.com>
2026-09-16 21:27:51 -0700

    Add Task_4.c for user input and display

---
+++ Task_4.c
@@ -0,0 +1,15 @@
+int main()
+{
+    char Name [20];
+    int roll;
+    char section;
+    printf("Enter A Name = ");
+    scanf("%s",Name);
+    printf("\nEnter A Roll = ");
+    scanf("%d",&roll);
+    printf("\nEnter A Section = ");
+    scanf(" %c",&section);
+    printf("Name =\"%s\"",Name);
+    printf("\nRoll Number : %d",roll);
+    printf("\nSection = %c",section);
+}
